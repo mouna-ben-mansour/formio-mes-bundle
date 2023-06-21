2d9b09bec0483700f6fc80233b49edf4f3d7453a
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-06-21 17:36:18 +0200

    xebpack config

--- bundle/Resources/encore/ibexa.config.js
+++ bundle/Resources/encore/ibexa.config.js
@@ -3,18 +3,14 @@ const path = require('path');
 module.exports = (Encore) => {
     Encore
     .addEntry('formio-style', [
-        path.resolve(__dirname, './public/bundles/formio/css/formio_dqe.css'),
+        path.resolve(__dirname, './public/css/formio_dqe.css'),
     ])
     .addEntry('formio-dqe', [
-        path.resolve(__dirname, `./public/bundles/formio/js/dqe.js`)
+        path.resolve(__dirname, `./public/js/dqe.js`)
     ])
     .addEntry('formio', [
-        path.resolve(__dirname, `./public/bundles/formio/js/formio.js`)
-    ])
-    /**
-     * Options
-     */
-    .configureCssLoader(options => { options.url = false })
+        path.resolve(__dirname, `./public/js/formio.js`)
+    ]);
 };
 
 
