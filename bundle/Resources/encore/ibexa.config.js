8084a3dd45399703c8267e7536dd28f268d59f54
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-06-21 17:39:37 +0200

    webpack config

--- bundle/Resources/encore/ibexa.config.js
+++ bundle/Resources/encore/ibexa.config.js
@@ -3,13 +3,13 @@ const path = require('path');
 module.exports = (Encore) => {
     Encore
     .addEntry('formio-style', [
-        path.resolve(__dirname, './public/css/formio_dqe.css'),
+        path.resolve(__dirname, '../public/css/formio_dqe.css'),
     ])
     .addEntry('formio-dqe', [
-        path.resolve(__dirname, `./public/js/dqe.js`)
+        path.resolve(__dirname, `../public/js/dqe.js`)
     ])
     .addEntry('formio', [
-        path.resolve(__dirname, `./public/js/formio.js`)
+        path.resolve(__dirname, `../public/js/formio.js`)
     ]);
 };
 
